ffc3abc7125987677ccb067f19a97404fc5e1f21
AshokACG <68427242+AshokACG@users.noreply.github.com>
2021-07-14 17:31:56 +0530

    new-changes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Fragment } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import ReduxExample from "./Components/SimpleReduxExample";
+// import ReduxExample from "./Components/SimpleReduxExample";
 // import Login from "./Components/LoginComponent";
 // import TaskComonent from "./Components/TaskComponent";
 // import ClickerComponent from "./Components/ClickerCompoent";
 // import MouseHoverComponent from "./Components/MouseHoverComponent";
 // import SimpleHooks from "./Components/simpleHooks";
-// import ApiCallComponent from "./Components/ApiCallComponent";
+import ApiCallComponent from "./Components/ApiCallComponent";
 
 function App() {
   return (
     <Fragment>
-      <ReduxExample />
+      <ApiCallComponent />
     </Fragment>
   );
 }
